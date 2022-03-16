1de5c79a600c34812b430c55bed882bcc2d85b5a
Belén Torrente <belentt@gmail.com>
2022-03-16 23:47:58 +0100

    New patch metrics and aux display function

--- growth_ans.py
+++ growth_ans.py
@@ -24,6 +24,8 @@ class Growth(DynamicModel):
         # State variables to store timestep-based results
         self.total_var = []
         self.total_skew = []
+        self.total_patch_count = []
+        self.avg_patch_size = []
 
     def initial(self):
         # maximum growth rate
@@ -49,6 +51,14 @@ class Growth(DynamicModel):
         a_uniform_map = uniform(1)
         self.report(a_uniform_map, 'uni')
 
+    def display_final(self, data, title, file_prefix):
+        print(title)
+        print(data)
+        pylab.title(title)
+        pylab.plot(data)
+        pylab.savefig(file_prefix + '_' + str(self.nrTimeSteps()) + '_' + str(self.map_len) + '.png')
+        pylab.show()
+
     def dynamic(self):
         growth = self.r * self.x * (1 - self.x / self.K) - self.c * ((self.x * self.x) / ((self.x * self.x) + 1))
         diffusion = self.D * (window4total(self.x) - self.numberOfNeighbours * self.x)
@@ -73,39 +83,47 @@ class Growth(DynamicModel):
         iter_skew = maptotal((self.x - iter_mean) ** 3) / cell_area
         self.total_skew.append(float(iter_skew))  # Store this variance for final results
 
-        self.report(iter_var, 'var')
-        self.report(iter_skew, 'skew')
         # Show results on screen
         print("iter=%d, mean=%f, variance=%f, std=%f, skewness=%f" % (self.currentTimeStep(), float(iter_mean), float(iter_var), iter_std, float(iter_skew)))
         # aguila(self.x)  # Plot this iteration of the model map
         # pylab.imshow(pcraster.pcr2numpy(self.x, 0))  # Uncomment to plot this iteration of model map (same as above)
         # pylab.show()  # Uncomment to plot this iteration of model map (same as above)
 
-        ft_raster = numpy.fft.fft2(pcraster.pcr2numpy(self.x, 0))
-        ft_raster = numpy.fft.fftshift(ft_raster)
-        pylab.imshow(numpy.abs(ft_raster)) # Uncomment to plot fft2
+        # ft_raster = numpy.fft.fft2(pcraster.pcr2numpy(self.x, 0))
+        # ft_raster = numpy.fft.fftshift(ft_raster)
+        # pylab.imshow(numpy.abs(ft_raster)) # Uncomment to plot fft2
         # pylab.show() # Uncomment to plot fft2
-        pylab.savefig('fft' + '_' + str(self.nrTimeSteps()) + '_' + str(self.map_len) + '_' +
-                      str(self.currentTimeStep()) + '.png')  # Save every fft run
+        # pylab.savefig('fft' + '_' + str(self.nrTimeSteps()) + '_' + str(self.map_len) + '_' +
+        #               str(self.currentTimeStep()) + '.png')  # Save every fft run
         # input("Press enter to continue") # Uncomment to plot one by one
+
+        # Calculations towards patch count
+        report(self.x, "temp.map")  # save state to temp file to manipulate it later
+        clump_map = clump(nominal(pcraster.readmap("temp.map")))
+        clump_matrix = numpy.asmatrix(pcraster.pcr2numpy(clump_map, -1))
+        min_clump_id = clump_matrix.min()
+        max_clump_id = clump_matrix.max()
+        self.total_patch_count.append(clump_matrix.max())
+        clump_sizes = []
+        for i in range(min_clump_id, max_clump_id):
+            curr_total = (clump_matrix == i).sum()
+            clump_sizes.append(curr_total)
+        if not clump_sizes:
+            self.avg_patch_size.append(self.map_len * self.map_len)
+        else:
+            self.avg_patch_size.append(numpy.matrix(clump_sizes).mean())
 
         # Export results on last iteration
         if self.currentTimeStep() == self.nrTimeSteps():
-            print("End of model run")
-            print("Variance over time:")
-            print(self.total_var)
             pylab.close()  # Close previous figure in case fft figure above is active
-            pylab.plot(self.total_var)
-            pylab.savefig('var' + '_' + str(self.nrTimeSteps()) + '_' + str(self.map_len) + '.png')
-            pylab.show()
-            print("Skewness over time:")
-            print(self.total_skew)
-            pylab.plot(self.total_skew)
-            pylab.savefig('skew' + '_' + str(self.nrTimeSteps()) + '_' + str(self.map_len) + '.png')
-            pylab.show()
+            print("End of model run")
+            self.display_final(self.total_var, "Variance over time", "var")
+            self.display_final(self.total_skew, "Skewness over time", "skew")
+            self.display_final(self.total_patch_count, "Patch count over time", "patch_count")
+            self.display_final(self.avg_patch_size, "Average patch size over time", "patch_size")
 
 
-nrOfTimeSteps = 7000
+nrOfTimeSteps = 2500
 cellSize = 40
 myModel = Growth(cellSize)
 dynamicModel = DynamicFramework(myModel, nrOfTimeSteps)
